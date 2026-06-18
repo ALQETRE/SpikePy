aadabb0828ebcfbc7c91ad00c7357f92e6222ffe
ALQETRE <alqetrea@gmail.com>
2026-06-18 12:49:08 +0200

    FINAL FLL 2025/26

--- hope.py
+++ hope.py
@@ -13,7 +13,7 @@ left_wheel = Wheel(left_port, wheel_rad)
 right_wheel = Wheel(right_port, wheel_rad)
 actuator = Actuator(actuator_port)
 
-bot = Robot(hub, left_wheel, right_wheel, axel_len, battery_low=7450, verbose= True)
+bot = Robot(hub, left_wheel, right_wheel, axel_len, battery_low=7450, verbose= False)
 
 
 def blue_track():
